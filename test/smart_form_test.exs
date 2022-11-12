6d8f4b050e6604971acf850c74c073f4fdd74a97
Jørgen Orehøj Erichsen <joe@erichsen.net>
2022-11-12 15:52:20 +0100

    Remove failing test

--- test/smart_form_test.exs
+++ test/smart_form_test.exs
@@ -2,10 +2,6 @@ defmodule SmartFormTest do
   use ExUnit.Case
   doctest SmartForm
 
-  test "greets the world" do
-    assert SmartForm.hello() == :world
-  end
-
   defmodule Form do
     use SmartForm
 
