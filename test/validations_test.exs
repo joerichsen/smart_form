b94a2bb4753d349dafbf1e6312d042149722f8ec
Jørgen Orehøj Erichsen <joe@erichsen.net>
2022-11-20 09:31:18 +0100

    Reorganized test code a bit

--- test/validations_test.exs
+++ test/validations_test.exs
@@ -1,24 +1,8 @@
-defmodule SmartFormTest do
+defmodule ValidationsTest do
   use SmartForm.DataCase
   doctest SmartForm
 
   alias SmartForm.User
-
-  describe "new" do
-    defmodule NewUserForm do
-      use SmartForm
-
-      fields do
-        field :firstname, :string
-      end
-    end
-
-    test "should accept a struct as an argument and make it the source" do
-      user = %User{firstname: "Marie"}
-      form = NewUserForm.new(user)
-      assert form.source == user
-    end
-  end
 
   describe "validate" do
     defmodule ValidateUserForm do
